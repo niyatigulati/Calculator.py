3c01dcb3143ddc00ce00b2bdcff48f8ecd63230d
Eufrasia <30950368+Eufras1a@users.noreply.github.com>
2019-10-13 17:01:27 +0530

    initial commit

---
+++ calc.py
@@ -0,0 +1,30 @@
+def calc(choice, x, y):
+    if (choice == 1):
+        add(x, y)
+    elif (choice == 2):
+        sub(x, y)
+    elif (choice == 3):
+        mul(x,y)
+    elif (choice == 2):
+        div(x,y)
+
+
+#  the below functions must display the output for the given arithmetic
+# TODO 
+def add(a, b):
+  pass
+  
+def sub(a, b):
+  pass
+  
+def mul(a, b):
+  pass
+  
+def div(a, b):
+  pass
+
+
+# TODO 
+# main function here 
+# Display options on what a calculator can do to perform (1 for add, 2 for sub and so on... )
+# This must be in a while loop
