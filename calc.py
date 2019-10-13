00bd37ee442d8f6f5b8c735df357d4e668b2bb2f
niyatigulati <43707920+niyatigulati@users.noreply.github.com>
2019-10-13 17:39:20 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -29,4 +29,4 @@ while(1):
     var1=int(input("Enter first number:"))
     var2=int(input("Enter second number:"))
     print("What do you want to do with these numbers ?")
-    print("1. add \n 2. sub \n 3. mul \n 4. div \n")
+    print("1. add \n2. sub \n3. mul \n4. div \n")
