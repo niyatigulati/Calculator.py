54ee3985b0170a0b82967b7166160bd5bcdbe078
Smriti <smritikpriya@gmail.com>
2019-10-22 16:17:00 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,11 +1,11 @@
 def calc(choice, x, y):
-    if (choice == 1):
+    if (choice == 'a'):
         add(x, y)
-    elif (choice == 2):
+    elif (choice == 's'):
         sub(x, y)
-    elif (choice == 3):
+    elif (choice == 'm'):
         mul(x,y)
-    elif (choice == 2):
+    elif (choice == 'd'):
         div(x,y)
 
 
@@ -23,8 +23,8 @@ def mul(a, b):
 def div(a, b):
   print(f'The division is {a/b}')
 
-
-# TODO 
-# main function here 
-# Display options on what a calculator can do to perform (1 for add, 2 for sub and so on... )
-# This must be in a while loop
+def main():
+    n=input('''input your choice \n"a" to add \n"s" to subtract \n"m" to multiply" \n"d" to divide' )
+    a=int(input("enter the first number"))
+    b=int(input("enter the second number"))
+    calc(n,a,b)
